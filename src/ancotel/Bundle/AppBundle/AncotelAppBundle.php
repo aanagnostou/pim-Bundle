b073a6df94ca913b9a2e986b8b7906ec988fa23e
aanagnostou <anag@ancotel.gr>
2018-06-12 13:06:57 +0300

    Create AncotelAppBundle.php

---
+++ src/ancotel/Bundle/AppBundle/AncotelAppBundle.php
@@ -0,0 +1,10 @@
+# /src/Acme/Bundle/AppBundle/AcmeAppBundle.php
+<?php
+
+namespace Ancotel\Bundle\AppBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AncotelAppBundle extends Bundle
+{
+}
