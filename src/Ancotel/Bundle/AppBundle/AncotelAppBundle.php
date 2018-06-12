83799410ccb3c1ba432973de004c300643232712
aanagnostou <anag@ancotel.gr>
2018-06-12 14:22:23 +0300

    add author, copyright, licence in AncotelAppBundle

--- src/Ancotel/Bundle/AppBundle/AncotelAppBundle.php
+++ src/Ancotel/Bundle/AppBundle/AncotelAppBundle.php
@@ -4,6 +4,13 @@
 namespace Ancotel\Bundle\AppBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+/**
+ * Ancotel application bundle
+ *
+ * @author    Anagnostou Antonios <anag@ancotel.gr>
+ * @copyright 2018 Ancotel (http://www.ancotel.gr)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 
 class AncotelAppBundle extends Bundle
 {
